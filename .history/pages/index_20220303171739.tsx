4120bd7c34d96f8537f4ecbbc92d244ca1fc0d41
Samm014 <samuel.medeiros014@gmail.com>
2022-03-03 17:20:11 -0300

    initial commit

---
+++ .history/pages/index_20220303171739.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h2>Hello, World!</h4>
+    </div>
+  )
+}
